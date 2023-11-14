99705b1623c294852b18bf5b3ae4f2966d44ad9a
Dragusin Cristian <dragusincristian609@gmail.com>
2023-11-14 18:07:33 +0200

    prj init

---
+++ app/src/main/java/com/example/testpractice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.testpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // 1. List Recycler View
+    // 2. Pass data with intents
+    // 3. Get the item image with API call
+}
